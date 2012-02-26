1d2f7604f5771dda58f0b08aa6c51deadbf1a6d2
Katie Cunningham <kcunning@katie-mintyhot.(none)>
2012-02-26 15:51:31 -0500

    And treasures now uses choices! Thanks, Marius Gedminas!

--- roguey/classes/items.py
+++ roguey/classes/items.py
@@ -8,7 +8,5 @@ class Treasure(object):
 	''' Not implemented yet. 
 	'''
 	def __init__(self):
-		k = ALL_TREASURES.keys()
-		r = random.randint(0, ALL_TREASURES.keys().__len__()-1)
-		self.title = ALL_TREASURES.keys()[r]
+		self.title = random.choice(ALL_TREASURES.keys())
 		self.description = ALL_TREASURES[self.title]
