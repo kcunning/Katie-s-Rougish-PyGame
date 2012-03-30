668fedf1368160654717d961719776fcffaaa7af
Katie Cunningham <kcunning@katie-mintyhot.(none)>
2012-03-29 21:15:41 -0400

    Not crashing, but FOV is now broken.

--- roguey/classes/game.py
+++ roguey/classes/game.py
@@ -45,62 +45,10 @@ class Game(object):
         self.screen.draw_stats(player_stats=self.player_stats)
         self.run()
 
-    def draw_stats(self, color=WHITE):
-        self.screen.blit(self.stats_screen, (1008, 0))
-        self.stats_screen = self.small_font.render(self.player_stats.name, True, color, BLACK)
-        self.screen.blit(self.stats_screen, (1008, 0))
-        self.stats_screen = self.small_font.render("Level: " + str(self.player_stats.level), True, color, BLACK)
-        self.screen.blit(self.stats_screen, (1008, 15))
-        self.stats_screen = self.small_font.render("HP: %s/%s" % (str(self.player_stats.current_hp), str(self.player_stats.max_hp)), True, color, BLACK)
-        self.screen.blit(self.stats_screen, (1008, 30)) 
-
-    def draw_inventory(self):
-        self.screen.blit(self.inventory_screen, (1008, 100))
-        items = self.inventory.get_items()
-        for i in range(items.__len__()):
-            line = self.small_font.render(LONG_STRING, True, BLACK, BLACK)
-            self.screen.blit(line, (1008, ((i+1)*15)+100))
-        pygame.display.flip()
-        for item in items:
-            line = self.small_font.render(item, True, WHITE, BLACK)
-            self.screen.blit(line, (1008, (items.index(item)+1)*15+100))
-        pygame.display.flip()
-
     def add_treasure(self, treasure):
         text = "You found a %s. %s" % (treasure.title, treasure.description)
         self.inventory.add_to_inventory(treasure.title)
-        self.draw_alert(text)
-
-    def draw_treasure(self):
-        ''' Draws the treasure chests yet to be opened.
-        '''
-        for row in range(ROWS):
-            for col in range(COLUMNS):
-                if self.map.treasure[row][col] != 0:
-                    treasure = pygame.image.load(IMG_DIR + 'chest.png')
-                    self.screen.blit(treasure, (row*TILE_SIZE, col*TILE_SIZE))
-    
-    def draw_monsters(self):
-        for row in range(ROWS):
-            for col in range(COLUMNS):
-                if self.map.monsters[row][col] != 0 and self.map.current[row][col] != 0:
-                    monster = pygame.image.load(IMG_DIR + 'dumb_monster.png')
-                    self.screen.blit(monster, (row*TILE_SIZE, col*TILE_SIZE))
-
-    def draw_darkness(self):
-        ''' Draws the darkness and shadows on the board. 0 is dark, 1 is in shadows,
-        '''
-        for row in range(ROWS):
-            for col in range(COLUMNS):
-                if self.map.cleared[row][col] == 0:
-                    if not self.map.current[row][col]:
-                        pygame.draw.rect(self.screen, BLACK, (row*TILE_SIZE, col*TILE_SIZE, TILE_SIZE, TILE_SIZE))  
-                if self.map.cleared[row][col] == 1:
-                    if not self.map.current[row][col]:
-                        shadow = pygame.Surface((TILE_SIZE, TILE_SIZE))
-                        shadow.set_alpha(200)
-                        shadow.fill(BLACK)
-                        self.screen.blit(shadow, (row*TILE_SIZE, col*TILE_SIZE))
+        self.screen.draw_alert(text)
 
     def has_wall(self, row, col):
         row = row/TILE_SIZE
@@ -144,10 +92,11 @@ class Game(object):
         treasure = self.map.clear_treasure(self.map.player)
         if treasure:
             self.add_treasure(treasure)
-            self.draw_inventory()
-        self.screen.blit(self.player_blit, self.map.player)
+            self.screen.draw_inventory(self.inventory)
+        self.screen.draw_player(self.map.player)
         self.draw_screen_layers()
-        self.screen.blit(self.player_blit, self.map.player)
+        self.screen.draw_player(self.map.player)
+        #self.screen.blit(self.player_blit, self.map.player)
         pygame.display.flip()
 
     def end_game(self):
@@ -158,31 +107,7 @@ class Game(object):
         self.screen.draw_walls(self.map.walls)
         self.screen.draw_monsters(self.map)
         self.screen.draw_darkness(self.map)
-        self.screen.draw_stats()
-
-    def animate_move(self, hor, vert, blit):
-        if vert:
-            if vert > 0:
-                for i in range(TILE_SIZE/MOVEMENT_SIZE):
-                    self.draw_screen_layers()
-                    self.screen.blit(self.__getattribute__(blit), [self.old_row, self.old_col+i*MOVEMENT_SIZE])
-                    pygame.display.update()
-            else:
-                for i in range(TILE_SIZE/MOVEMENT_SIZE):
-                    self.draw_screen_layers()
-                    self.screen.blit(self.__getattribute__(blit), [self.old_row, self.old_col-i*MOVEMENT_SIZE])
-                    pygame.display.update()
-        if hor:
-            if hor > 0:
-                for i in range(TILE_SIZE/MOVEMENT_SIZE):
-                    self.draw_screen_layers()
-                    self.screen.blit(self.__getattribute__(blit), [self.old_row+i*MOVEMENT_SIZE, self.old_col])
-                    pygame.display.update()
-            else:
-                for i in range(TILE_SIZE/MOVEMENT_SIZE):
-                    self.draw_screen_layers()
-                    self.screen.blit(self.__getattribute__(blit), [self.old_row-i*MOVEMENT_SIZE, self.old_col])
-                    pygame.display.update()
+        self.screen.draw_stats(player_stats=self.player_stats)
 
     def run(self):
         ''' The main loop of the game.
