98e7e7152648718a96079c5c107922cd7a833c18
Doug Napoleone <Doug.Napoleone@gmail.com>
2012-04-08 15:15:59 -0400

    some minor module import changes.

--- main.py
+++ main.py
@@ -2,19 +2,17 @@
 import pygame, math, sys, random
 from pygame.locals import *
 
-sys.path.append("roguey/classes")
-
-from constants import *
-from items import Treasure
-from gamemap import Map
-from monsters import Monster
-from player import Inventory
-from game import Game
+from roguey.classes.constants import *
+from roguey.classes.items import Treasure
+from roguey.classes.gamemap import Map
+from roguey.classes.monsters import Monster
+from roguey.classes.player import Inventory
+from roguey.classes.game import Game
 
 def main():
-        while 1:
-		pygame.init()
-		game = Game()
+    while 1:
+        pygame.init()
+        game = Game()
 
 if __name__ == "__main__":
         main()
